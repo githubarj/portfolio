d9088c7a84852a65fc9794e60dc2911b2250320b
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2023-01-25 23:15:43 +0300

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import Header from './Components/header/Header'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
-    </div>
+   <>
+   <Header/>
+   </>
   )
 }
 
