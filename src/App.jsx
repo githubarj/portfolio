4e1e8f98affb26a8e000d690305f7e79125db605
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2023-01-26 03:13:26 +0300

    home halfway done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Header from './Components/header/Header'
+import Home from './Components/home/Home'
 
 
 function App() {
 
   return (
-   <>
-   <Header/>
-   </>
-  )
+    <>
+      <Header />
+      <main className="main">
+        <Home />
+      </main>
+    </>
+  );
 }
 
 export default App
