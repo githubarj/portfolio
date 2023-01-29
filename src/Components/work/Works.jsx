48da53c9e4bc68302f80ec4b31657b7f9a42b35b
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2023-01-29 23:22:39 +0300

    done

--- src/Components/work/Works.jsx
+++ src/Components/work/Works.jsx
@@ -12,14 +12,15 @@ const Works = () => {
       setProjects(projectsData);
     } else {
       const newProjects = projectsData.filter((project) => {
-        return project.category === item.name;
+        return project.category.toLowerCase() === item.name;
       });
       setProjects(newProjects);
     }
   }, [item]);
 
   const handleClick = (e, index) => {
-    setItem({ name: e.target.textContent });
+    setItem({ name: e.target.textContent.toLowerCase() });
+    setActive(index)
   };
 
   return (
@@ -31,7 +32,9 @@ const Works = () => {
               onClick={(e) => {
                 handleClick(e, index);
               }}
-              className="work__item"
+              className={
+                `${ active === index ? "active-work" : "" } work__item`
+              }
               key={index}
             >
               {item.name}
