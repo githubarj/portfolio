7c908e2d432e7bd1477480d963c533548fc0b254
Richard Jeremy Githuba <githuba9520@gmail.com>
2023-09-10 11:29:51 +0300

    added works

--- src/Components/work/WorkItems.jsx
+++ src/Components/work/WorkItems.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const WorkItems = ( {item} ) => {
+const WorkItems = ( {item} ) => { 
   return (
     <div className="work__card" key={item.id} >
         <img src={item.image} alt="" className="work__img" />
         <h3 className="work__title"> {item.title} </h3>
         <a href={item.link} className="work__button" target= "_blank">
-            Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+            {item.tag} <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
 
     </div>
--- src/Components/work/worksData.js
+++ src/Components/work/worksData.js
@@ -1,4 +1,3 @@
-import Work1 from "../../assets/work1.jpg"
 import Work2 from "../../assets/work2.jpg"
 import Work3 from "../../assets/work3.jpg"
 import Work4 from "../../assets/work4.jpg"
@@ -7,17 +6,27 @@ import Work5 from "../../assets/work5.jpg";
 export const projectsData = [
   {
     id: 1,
-    image: Work1,
-    title: "Perfect square",
-    link: "https://perfectsquare.cc",
-    category: "web",
+    image: "/worksImages/perfect-square-v2.png",
+    title: "Perfect square V2",
+    link: "https://creative-blancmange-4db58b.netlify.app/",
+    category: "live",
+    tag: "Live site",
   },
   {
     id: 2,
-    image: Work2,
-    title: "Disney + Clone",
-    link: " https://disneyplus-7a180.web.app/",
-    category: "app",
+    image: "/worksImages/changia.png",
+    title: "Changia",
+    link: "https://glistening-gumption-23fb60.netlify.app/",
+    category: "live",
+    tag: "Live site",
+  },
+  {
+    id: 6,
+    image: "/worksImages/perfect-square-v1.png",
+    title: "Perfect Square v1",
+    link: " https://perfectsquare.cc/",
+    category: "live",
+    tag: "Live site",
   },
   {
     id: 3,
@@ -25,6 +34,7 @@ export const projectsData = [
     title: " Blockchain-Covid-Certificates ",
     link: "https://github.com/githubarj/Blockchain-Covid-Certificates",
     category: "blockchain",
+    tag: "Repository",
   },
   {
     id: 4,
@@ -32,13 +42,39 @@ export const projectsData = [
     title: "Hardhat Smart Contract Lottery",
     link: "https://github.com/githubarj/hardhat-smartcontract-lottery",
     category: "blockchain",
+    tag: "Repository",
   },
   {
     id: 5,
+    image: "/worksImages/airbnb-experiences.png",
+    title: "Airbnb Experiences",
+    link: "https://thunderous-faloodeh-043d1a.netlify.app/",
+    category: "practice",
+    tag: "Demo",
+  },
+  {
+    id: 7,
+    image: "/worksImages/travel-journal.png",
+    title: "Travel Journal",
+    link: "https://lucent-basbousa-af899a.netlify.app/ ",
+    category: "practice",
+    tag: "Demo",
+  },
+  {
+    id: 8,
+    image: "/worksImages/mem-generator.png",
+    title: "Meme Generator",
+    link: " https://charming-piroshki-1827e3.netlify.app/ ",
+    category: "practice",
+    tag: "Demo",
+  },
+  {
+    id: 9,
     image: Work5,
     title: "Melodius",
     link: " https://github.com/githubarj/Melodius_final ",
     category: "blockchain",
+    tag: "Repository",
   },
 ];
 
@@ -47,10 +83,10 @@ export const projectsNav = [
     name: "all",
   },
   {
-    name: "web",
+    name: "live",
   },
   {
-    name: "app",
+    name: "practice",
   },
   {
     name: "blockchain",
